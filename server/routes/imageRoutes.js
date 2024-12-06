ef99b10bb7240000a79fd77dd50b25f162529ea3
Lance <ljpruitt123@gmail.com>
2024-12-06 10:50:24 -0800

    fix: Auth.js to auth.js in imageRoutes.js

--- server/routes/imageRoutes.js
+++ server/routes/imageRoutes.js
@@ -1,6 +1,6 @@
 import express from "express";
 import { generateImage } from "../controllers/imageController.js";
-import userAuth from "../middlewares/Auth.js";
+import userAuth from "../middlewares/auth.js";
 
 const imageRouter = express.Router();
 
