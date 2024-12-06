b70616cadca6d88d8fa057b32f547fdada344fc4
Lance <ljpruitt123@gmail.com>
2024-12-06 10:47:57 -0800

    fix: import Auth.js to auth.js for vercel deployment

--- server/routes/userRoutes.js
+++ server/routes/userRoutes.js
@@ -1,6 +1,6 @@
 import express from 'express'
 import { registerUser, loginUser, userCredits } from '../controllers/userController.js'
-import userAuth from '../middlewares/Auth.js'
+import userAuth from '../middlewares/auth.js'
 
 const userRouter = express.Router()
 
